e6bb868e26bc6d5219478c906af33a5932ba836c
wadanaoumi <wada121400@gmail.com>
2021-10-27 14:32:31 +0900

    ログイン画面実装

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,12 @@
+import { AuthApi, Configuration } from '@/app/apiclient';
+import { environment } from '@/environments/environment';
+import { Overlay } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSpinner } from '@angular/material/progress-spinner';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +14,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  formGroup = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+    ]),
+  });
 
-  constructor() { }
+  overlayRef = this.overlay.create({
+    hasBackdrop: true,
+    positionStrategy: this.overlay
+      .position().global().centerHorizontally().centerVertically()
+  });
+
+  constructor(public overlay: Overlay, public router: Router, public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
+  /**
+   * フォームで入力されたメールアドレス情報を取得
+   */
+  public get email() {
+    return <FormControl>this.formGroup.get('email');
+  }
+
+  /**
+   * フォームで入力されたパスワード情報を取得
+   */
+  public get password() {
+    return <FormControl>this.formGroup.get('password');
+  }
+
+  /**
+   * ログインAPIにリクエストする
+   */
+  public async submit() {
+    if (!this.formGroup.valid) {
+      // バリデーションエラーが存在する場合
+      return;
+    }
+
+    this.overlayRef.attach(new ComponentPortal(MatSpinner));
+
+    try {
+      const response = await new AuthApi(new Configuration({ basePath: environment.apiUrl })).login({
+        email: this.formGroup.get('email')?.value,
+        password: this.formGroup.get('password')?.value,
+      });
+
+      if (response.status !== 200) throw new Error();
+
+      this.router.navigateByUrl('/');
+    } catch (e) {
+      console.error(e);
+
+      this.snackBar.open('ログインに失敗しました', '閉じる', { verticalPosition: 'top' });
+
+      this.email.setErrors({ faildLogin: true });
+      this.password.setErrors({ faildLogin: true });
+    }
+
+    this.overlayRef.detach();
+  }
 }
