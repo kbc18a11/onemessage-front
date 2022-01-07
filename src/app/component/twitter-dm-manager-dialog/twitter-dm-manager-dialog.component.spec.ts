53f91cfc0a7a8afb42840d356187f67ab908d606
wadanaoumi <wada121400@gmail.com>
2022-01-07 22:21:55 +0900

    不要なimportを削除

--- src/app/component/twitter-dm-manager-dialog/twitter-dm-manager-dialog.component.spec.ts
+++ src/app/component/twitter-dm-manager-dialog/twitter-dm-manager-dialog.component.spec.ts
@@ -2,7 +2,6 @@ import { OverlayModule } from '@angular/cdk/overlay';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { TwitterDmManagerDialogComponent } from './twitter-dm-manager-dialog.component';
