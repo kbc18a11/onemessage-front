f834689bf4c0c888604a493dd967fe3a9c634c7b
wadanaoumi <wada121400@gmail.com>
2022-01-29 21:52:41 +0900

    本番環境のバックエンドのurlを修正

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://54.199.67.22:8080/api/v1',
+  apiUrl: 'http://54.95.28.12:8080/api/v1',
   FREA_BASE_APP_ID: "1:75238837761:web:c8863443d8145fffe7fa00",
   FREA_BASE_APIKEY: "AIzaSyCfJ0odp2ft7gB-ojpX5xJLnqKZmz3agkU",
   FREA_BASE_AUTH_DOMAIN: "one-message-dec2c.firebaseapp.com",
