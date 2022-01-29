04f49432f6ed5092e224189b2faf53ab6200b6b5
wadanaoumi <wada121400@gmail.com>
2022-01-30 00:48:53 +0900

    ホーム画面にdm送信履歴機能の実装

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,9 @@
+import { Configuration, DmApi, GetDmResponseMessages } from '@/app/apiclient';
+import { AuthService } from '@/app/services/auth/auth.service';
+import { environment } from '@/environments/environment';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +11,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  dmHistories: GetDmResponseMessages[] = [];
 
-  constructor() { }
+  constructor(
+    public router: Router,
+    public snackBar: MatSnackBar,
+    public authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.getDmHistory();
   }
 
+
+  /**
+   * dm送信履歴を取得する
+   *
+   * @returns
+   */
+  async getDmHistory() {
+    try {
+      const response = await new DmApi(new Configuration({ basePath: environment.apiUrl, apiKey: this.authService.token }))
+        .getDm();
+
+      if (response.status !== 200) throw new Error();
+
+      this.dmHistories = response.data.messages;
+    } catch (e) {
+      console.error(e);
+
+      if (e.response.status === 401) {
+        this.authService.logout();
+        this.router.navigateByUrl('/login');
+        return;
+      }
+
+      this.snackBar.open(
+        'メッセージを履歴を取得することができませんでした。大変申し訳ございませんが、画面を再読み込みを行ってください。'
+        + 'それでも解除できない場合は、お手数をおかけいたしますが、運営に問い合わせをよろしくお願いします。',
+        '閉じる',
+        { verticalPosition: 'top' }
+      );
+    }
+  }
+
+  dateTimeFormatter(dateTimeString: string) {
+    const dateTime = new Date(dateTimeString);
+
+    return `${dateTime.getFullYear()}年${dateTime.getMonth()}月${dateTime.getDay()}日${dateTime.getHours()}時${dateTime.getMinutes()}分`
+  }
 }
